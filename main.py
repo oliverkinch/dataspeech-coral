fd31acd6ebcd011d874fccd56d2e1585be786085
Oliver Kinch <oliver.kinch@gmail.com>
2024-09-19 11:30:13 +0200

    Steps 1 and 2 done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset, Audio
+from datasets import load_dataset, Audio, Dataset, concatenate_datasets
 from multiprocess import set_start_method
 from dataspeech import rate_apply, pitch_apply, snr_apply, squim_apply
 import torch
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     if args.configuration:
-        dataset = load_dataset(args.dataset_name, args.configuration, num_proc=args.cpu_num_workers,)
+        dataset = load_dataset(args.dataset_name, args.configuration, num_proc=args.cpu_num_workers, split="train")
     else:
-        dataset = load_dataset(args.dataset_name, num_proc=args.cpu_num_workers,)
+        dataset = load_dataset(args.dataset_name, num_proc=args.cpu_num_workers, split="train")
         
     audio_column_name = "audio" if args.rename_column else args.audio_column_name
     text_column_name = "text" if args.rename_column else args.text_column_name
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         squim_dataset_name = f"{args.dataset_name}-squim"
         # load from hub if available
         try:
-            squim_dataset = load_dataset(squim_dataset_name, num_proc=args.cpu_num_workers,)
+            squim_dataset = load_dataset(squim_dataset_name, num_proc=args.cpu_num_workers, split="train")
         except:
             squim_dataset = dataset.map(
                 squim_apply,
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     print("Compute pitch")
     pitch_dataset_name = f"{args.dataset_name}-pitch"
     try:
-        pitch_dataset = load_dataset(pitch_dataset_name, num_proc=args.cpu_num_workers,)
+        pitch_dataset = load_dataset(pitch_dataset_name, num_proc=args.cpu_num_workers, split="train")
     except:
         pitch_dataset = dataset.cast_column(audio_column_name, Audio(sampling_rate=16_000)).map(
             pitch_apply,
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     print("Compute snr and reverb")
     sns_dataset_name = f"{args.dataset_name}-snr"
     try:
-        snr_dataset = load_dataset(sns_dataset_name, num_proc=args.cpu_num_workers,)
+        snr_dataset = load_dataset(sns_dataset_name, num_proc=args.cpu_num_workers, split="train")
     except:
         snr_dataset = dataset.map(
             snr_apply,
@@ -97,21 +97,41 @@ if __name__ == "__main__":
     
     print("Compute speaking rate")
     rate_dataset_name = f"{args.dataset_name}-rate"
+
+
+    indices = list(range(len(dataset)))
+    n_batches = 10
+    batch_size = len(indices) // n_batches
+
+    CHARACTER_MAPS = {
+    "î": "i",
+    "İ": "i",
+    }
     try:
-        rate_dataset = load_dataset(rate_dataset_name, num_proc=args.cpu_num_workers,)
+        rate_dataset_merged = load_dataset(rate_dataset_name, num_proc=args.cpu_num_workers, split="train")
     except:
-        if "speech_duration" in snr_dataset[next(iter(snr_dataset.keys()))].features:  
-            print("Using speech duration")  
-            rate_dataset = dataset.map(
-                rate_apply,
-                with_rank=False,
-                num_proc=args.cpu_num_workers,
-                writer_batch_size= args.cpu_writer_batch_size,
-                fn_kwargs={"audio_column_name": audio_column_name, "text_column_name": text_column_name},
-            )
-        else:
-            print("No speech duration, using default")
-            rate_dataset = dataset.map(
+        print("Using speech duration")
+
+        for i in range(n_batches):
+            if i == n_batches - 1:
+                batch_indices = indices[i*batch_size:]
+            else:
+                batch_indices = indices[i*batch_size:(i+1)*batch_size]
+            dataset_i = dataset.select(batch_indices)
+
+            texts = dataset_i[text_column_name]
+            texts_mapped = ["" for _ in range(len(texts))]
+            for j in range(len(texts)):
+                text = texts[j]
+                for k, v in CHARACTER_MAPS.items():
+                    text = text.replace(k, v)
+                texts_mapped[j] = text
+
+            # use texts_mapped instead of texts in dataset_i
+            dataset_i = dataset_i.remove_columns([text_column_name]).add_column("text", texts_mapped)
+
+
+            rate_dataset = dataset_i.map(
                 rate_apply,
                 with_rank=False,
                 num_proc=args.cpu_num_workers,
@@ -119,17 +139,22 @@ if __name__ == "__main__":
                 remove_columns=[audio_column_name], # tricks to avoid rewritting audio
                 fn_kwargs={"audio_column_name": audio_column_name, "text_column_name": text_column_name},
             )
+            if i == 0:
+                rate_dataset_merged = rate_dataset
+            else:
+                rate_dataset_merged = concatenate_datasets([rate_dataset_merged, rate_dataset])
+            print(f"Dataset rate merge length: {len(rate_dataset_merged)}")
         # push to hub
-        rate_dataset.push_to_hub(rate_dataset_name)
+        rate_dataset_merged.push_to_hub(rate_dataset_name)
     
-    for split in dataset.keys():
-        dataset[split] = pitch_dataset[split].add_column("snr", snr_dataset[split]["snr"]).add_column("c50", snr_dataset[split]["c50"])
-        if "speech_duration" in snr_dataset[split]:
-            dataset[split] = dataset[split].add_column("speech_duration", snr_dataset[split]["speech_duration"])
-        dataset[split] = dataset[split].add_column("speaking_rate", rate_dataset[split]["speaking_rate"]).add_column("phonemes", rate_dataset[split]["phonemes"])
-        if args.apply_squim_quality_estimation:
-            dataset[split] = dataset[split].add_column("stoi", squim_dataset[split]["stoi"]).add_column("si-sdr", squim_dataset[split]["sdr"]).add_column("pesq", squim_dataset[split]["pesq"])
-    
+
+    dataset= pitch_dataset.add_column("snr", snr_dataset["snr"]).add_column("c50", snr_dataset["c50"])
+
+    dataset = dataset.add_column("speech_duration", snr_dataset["speech_duration"])
+    dataset = dataset.add_column("speaking_rate", rate_dataset_merged["speaking_rate"]).add_column("phonemes", rate_dataset_merged["phonemes"])
+    if args.apply_squim_quality_estimation:
+        dataset = dataset.add_column("stoi", squim_dataset["stoi"]).add_column("si-sdr", squim_dataset["sdr"]).add_column("pesq", squim_dataset["pesq"])
+
     if args.output_dir:
         print("Saving to disk...")
         dataset.save_to_disk(args.output_dir)
--- steps/step2.sh
+++ steps/step2.sh
@@ -1,6 +1,6 @@
 python ./scripts/metadata_to_text.py \
-    "oliverkinch/coral-tts-filtered-tags-small" \
-    --repo_id "oliverkinch/coral-tts-filtered-tags-small" \
+    "oliverkinch/coral-tts-filtered-tags" \
+    --repo_id "oliverkinch/coral-tts-filtered-tags" \
     --configuration "default" \
     --cpu_num_workers "8" \
     --path_to_bin_edges "./examples/tags_to_annotations/v02_bin_edges.json" \
